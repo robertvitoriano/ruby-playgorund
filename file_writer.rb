a036df814c6b62de99e8f86081c5c447de20c039
Robert Vitoriano <robertvitoriano@gmail.com>
2022-09-14 07:51:30 -0300

    remove unnecessary code

--- file_writer.rb
+++ file_writer.rb
@@ -1,8 +1,3 @@
 File.open("file_to_be_written.txt", "w") do |file|
   file.write("Sabrina, accounting")
 end
-
-# second way
-file = File.open("file_to_be_read.txt", "r")
-puts("SECOND WAY")
-puts file.read
