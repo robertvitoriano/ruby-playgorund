23244ee9d3028a2e08809a9a967bbb87b15ba172
Robert Vitoriano <robertvitoriano@gmail.com>
2022-09-14 10:52:54 -0300

    add inheritance

---
+++ inheritance.rb
@@ -0,0 +1,24 @@
+class Chef
+  def make_chicken
+    return  "The chef makes the chicken"
+  end
+  def make_salad
+    return  "The chef makes the salad"
+  end
+  def make_sushi
+    return  "The chef makes the sushi"
+  end
+end
+
+class BrazilianChef < Chef
+  def make_feijoada
+    return "The chef makes feijoada"
+  end
+end
+
+chef = Chef.new()
+brazilian_chef = BrazilianChef.new()
+
+puts("CHEF "+chef.make_chicken())
+puts("Brazilian Chef "+brazilian_chef.make_feijoada())
+puts("Brazilian Chef "+brazilian_chef.make_sushi())
